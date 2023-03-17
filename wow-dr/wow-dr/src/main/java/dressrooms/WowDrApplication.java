2920a6d6483f9ae32f4f324ac254efdd33ee14c9
Ivan Rodriguez <ivan.rodriguez.santos@outlook.com>
2023-03-17 23:47:02 +0100

    Initial commit
    Clean project pron Spring initializr

---
+++ wow-dr/wow-dr/src/main/java/dressrooms/WowDrApplication.java
@@ -0,0 +1,13 @@
+package dressrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowDrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowDrApplication.class, args);
+	}
+
+}

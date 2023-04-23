3d86d10b4dec72ed0efd6146a67782ace96c6037
Ivan Rodriguez <ivan.rodriguez.santos@outlook.com>
2023-04-23 23:05:49 +0200

    Create nav template, change to JpaRepository

--- wow-dr/src/main/java/dressrooms/repository/ItemsRepository.java
+++ wow-dr/src/main/java/dressrooms/repository/ItemsRepository.java
@@ -1,8 +1,8 @@
 package dressrooms.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import dressrooms.model.Item;
 
-public interface ItemsRepository extends CrudRepository<Item, Integer> {
+public interface ItemsRepository extends JpaRepository<Item, Integer> {
 
 }

1ffffd6f4c6f1619d787f27f765d0319c75b9e5f
Ivan Rodriguez <ivan.rodriguez.santos@outlook.com>
2023-08-19 17:43:38 +0200

    Refactor WowDrApp imports

--- wow-dr/src/main/java/dressrooms/WowDrApplication.java
+++ wow-dr/src/main/java/dressrooms/WowDrApplication.java
@@ -1,10 +1,9 @@
 package dressrooms;
 
 import java.util.Date;
-
 import java.util.List;
-
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;

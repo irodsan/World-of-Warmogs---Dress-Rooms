ddcf07df6fa37c7664c40e8eb3b7da11bc42d533
Ivan Rodriguez <ivan.rodriguez.santos@outlook.com>
2023-08-24 19:17:11 +0200

    First version createTransmog

--- wow-dr/src/main/java/dressrooms/controller/TransmogController.java
+++ wow-dr/src/main/java/dressrooms/controller/TransmogController.java
@@ -1,13 +1,23 @@
 package dressrooms.controller;
 
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import dressrooms.model.Classe;
 import dressrooms.model.Transmog;
+import dressrooms.model.User;
+import dressrooms.repository.ClasseRepository;
+import dressrooms.repository.TransmogRepository;
+import dressrooms.repository.UserRepository;
 import dressrooms.service.IItemService;
 import dressrooms.service.ITransmogService;
 import dressrooms.service.IUserService;
@@ -22,6 +32,15 @@ public class TransmogController {
     @Autowired
     public IUserService userService;
 
+    @Autowired
+    public ClasseRepository classeRepository;
+
+    @Autowired
+    public TransmogRepository transmogRepository;
+
+    @Autowired
+    public UserRepository userRepository;
+
     @GetMapping("transmog/showTransmog/{id}")
     public String mostrarTransmog(@PathVariable int id, Model model) {
         Transmog t = mostrarConjunto(id);
@@ -30,7 +49,21 @@ public class TransmogController {
     }
 
     @GetMapping("/transmog/createTransmog")
-    public String mostrarCreateTransmog(Model model) {
+    public String mostrarCreateTransmog(@ModelAttribute Transmog transmog) {
+        return "transmog/createTransmog";
+    }
+
+    @PostMapping("/transmog/createTransmog")
+    public String saveTransmog(Authentication auth, Transmog transmog) {
+        Optional<Classe> c = classeRepository.findById(4);
+        transmog.setClase(c.get());
+        transmog.setFecha(new Date());
+        System.out.println("Nombre: " + transmog.getNombre());
+        System.out.println("Alias del usuario: " + auth.getName());
+        User u = userRepository.findByAlias(auth.getName());
+
+        transmog.setId_usuario(u.getId());
+        transmogRepository.save(transmog);
         return "transmog/createTransmog";
     }
 
--- wow-dr/src/main/java/dressrooms/model/Transmog.java
+++ wow-dr/src/main/java/dressrooms/model/Transmog.java
@@ -3,6 +3,8 @@ package dressrooms.model;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -13,6 +15,7 @@ import jakarta.persistence.Table;
 public class Transmog {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private Date fecha;

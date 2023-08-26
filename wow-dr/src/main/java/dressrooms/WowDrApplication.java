5a9caeeb1abaf2d219c0035d3e37f4f52098e0bc
Ivan Rodriguez <ivan.rodriguez.santos@outlook.com>
2023-08-26 19:20:21 +0200

    Clean not used libs

--- wow-dr/src/main/java/dressrooms/WowDrApplication.java
+++ wow-dr/src/main/java/dressrooms/WowDrApplication.java
@@ -1,9 +1,5 @@
 package dressrooms;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;

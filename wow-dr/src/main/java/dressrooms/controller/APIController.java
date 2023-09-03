f6c822991bf9cc102899b18a88be70cf6ef1963e
Ivan Rodriguez <ivan.rodriguez.santos@outlook.com>
2023-09-03 16:25:06 +0200

    Added load items & icons with spring

--- wow-dr/src/main/java/dressrooms/controller/APIController.java
+++ wow-dr/src/main/java/dressrooms/controller/APIController.java
@@ -1,10 +1,17 @@
 package dressrooms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import dressrooms.API.ItemIcon;
+import dressrooms.model.ItemIcon;
 import dressrooms.API.ItemIconResponse;
 import dressrooms.API.ItemResponse;
+import dressrooms.model.Item;
+import dressrooms.repository.ItemsIconsRepository;
+import dressrooms.repository.ItemsRepository;
 import dressrooms.API.ItemAPI;
 
 import com.google.gson.Gson;
@@ -20,26 +27,56 @@ import java.sql.Statement;
 @Controller
 public class APIController {
 
-    public static void cargarItem(Integer inicio, Integer fin, String token) throws Exception {
+    @Autowired
+    private ItemsRepository itemsRepository;
+
+    @Autowired
+    private ItemsIconsRepository itemsIconsRepository;
+
+    @PostMapping("/cargarItem")
+    public String cargarItem(@RequestParam("inicio") Integer inicio, @RequestParam("fin") Integer fin,
+            @RequestParam("token") String token, Model model) throws Exception {
 
         for (int id = inicio; id < fin; id++) {
             System.out.println("PROCESANDO ID: " + id);
-            // String token = "EUdtf1IPJJ2zgz1FnlJINlc1QQWYd288gA";
-            String URL = "https://eu.api.blizzard.com/data/wow/media/item/" + id
+            // String token = "EUJ5P77APdOB4gS6spOFqTQWGIuclJhYMK";
+            String URL_ITEM = "https://eu.api.blizzard.com/data/wow/item/" + id
                     + "?namespace=static-eu&locale=es_ES&access_token=" + token;
-            String jsonResponse = peticionHttpGet(URL);
+            String jsonResponse = peticionHttpGet(URL_ITEM);
 
             if (!jsonResponse.equals("")) {
                 Gson gson = new Gson();
                 ItemResponse ir = gson.fromJson(jsonResponse, ItemResponse.class);
 
-                ItemAPI it = new ItemAPI(ir.getId(), ir.getName(), ir.getInventory_type().getType(),
+                Item it = new Item(ir.getId(), ir.getName(), ir.getInventory_type().getType(),
                         ir.getItem_subclass().getName(), ir.getQuality().getType());
-
                 System.out.println(it);
+
+                System.out.println(it.getNombre());
                 if (!it.getRanura().equals("NON_EQUIP")) {
-                    // saveItem(it);
+                    // itemsRepository.save(it);
+                    System.out.println("ITEM: " + it.getNombre());
                 }
+
+                String URL_ICON = "https://eu.api.blizzard.com/data/wow/media/item/" + id
+                        + "?namespace=static-eu&locale=es_ES&access_token=" + token;
+
+                System.out.println(URL_ICON);
+                System.out.println(
+                        "https://eu.api.blizzard.com/data/wow/media/item/208193?namespace=static-eu&locale=es_ES&access_token=EUJ5P77APdOB4gS6spOFqTQWGIuclJhYMK");
+                jsonResponse = peticionHttpGet(URL_ICON);
+
+                if (!jsonResponse.equals("")) {
+                    gson = new Gson();
+                    ItemIconResponse iir = gson.fromJson(jsonResponse, ItemIconResponse.class);
+
+                    if (iir.getAssets() != null) {
+                        ItemIcon ii = new ItemIcon(iir.getId(), iir.getAssets()[0].getValue());
+                        itemsIconsRepository.save(ii);
+                        System.out.println("ICON: " + ii.getUrl());
+                    }
+                }
+
             }
 
             if (id % 10 == 0) {
@@ -50,38 +87,12 @@ public class APIController {
                 }
             }
         }
+        return "redirect:/loadFromAPI";
     }
 
-    public static void cargarItemIcon(Integer inicio, Integer fin, String token) throws Exception {
-        for (int id = inicio; id < fin; id++) {
-            System.out.println("PROCESANDO ID: " + id);
-            String jsonResponse = peticionHttpGet("https://eu.api.blizzard.com/data/wow/media/item/" + id
-                    + "?namespace=static-eu&locale=es_ES&access_token=EUnpU5Psx7K91296wSmkbTLmQtK8Vdz4Dw");
-
-            if (!jsonResponse.equals("")) {
-                Gson gson = new Gson();
-                ItemIconResponse iir = gson.fromJson(jsonResponse, ItemIconResponse.class);
-
-                if (iir.getAssets() != null) {
-                    ItemIcon ii = new ItemIcon(iir.getId(), iir.getAssets()[0].getValue());
-                    saveItemIcon(ii);
-                }
-            }
-
-            if (id % 10 == 0) {
-                try {
-                    Thread.sleep(2000); // Wait for 2 seconds
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-    }
-
-    public static String peticionHttpGet(String urlParaVisitar) throws Exception {
+    public String peticionHttpGet(String urlAPI) throws Exception {
         StringBuilder resultado = new StringBuilder();
-        URL url = new URL(urlParaVisitar);
+        URL url = new URL(urlAPI);
         HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
         conexion.setRequestMethod("GET");
 
@@ -97,37 +108,75 @@ public class APIController {
         return resultado.toString();
     }
 
-    public static void saveItem(ItemAPI it) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wowdr", "root", "");
-            Statement stmt = con.createStatement();
-            String q = "INSERT INTO `items_icons`(`id`, `url`) VALUES ('" + it.getId() + "','" + it.getNombre() + "','"
-                    + it.getRanura() + "','" + it.getTipo() + "','" + it.getQuality() + "')";
-
-            // int rs = stmt.executeUpdate(q);
-            stmt.executeUpdate(q);
-
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    public static void saveItemIcon(ItemIcon ii) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wowdr", "root", "");
-            Statement stmt = con.createStatement();
-            String q = "INSERT INTO `items_icons`(`id`, `url`) VALUES ('" + ii.getId() + "','" + ii.getUrl() + "')";
-
-            // int rs = stmt.executeUpdate(q);
-            stmt.executeUpdate(q);
-
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+    /*
+     * public void cargarItemIcon(Integer inicio, Integer fin, String token) throws
+     * Exception {
+     * for (int id = inicio; id < fin; id++) {
+     * System.out.println("PROCESANDO ID: " + id);
+     * String jsonResponse =
+     * peticionHttpGet("https://eu.api.blizzard.com/data/wow/media/item/" + id
+     * +
+     * "?namespace=static-eu&locale=es_ES&access_token=EUnpU5Psx7K91296wSmkbTLmQtK8Vdz4Dw"
+     * );
+     * 
+     * if (!jsonResponse.equals("")) {
+     * Gson gson = new Gson();
+     * ItemIconResponse iir = gson.fromJson(jsonResponse, ItemIconResponse.class);
+     * 
+     * if (iir.getAssets() != null) {
+     * ItemIcon ii = new ItemIcon(iir.getId(), iir.getAssets()[0].getValue());
+     * //itemsIconsRepository.save(ii);
+     * }
+     * }
+     * 
+     * if (id % 10 == 0) {
+     * try {
+     * Thread.sleep(2000); // Wait for 2 seconds
+     * } catch (InterruptedException e) {
+     * e.printStackTrace();
+     * }
+     * }
+     * }
+     * 
+     * }
+     * 
+     * /*
+     * public void saveItem(ItemAPI it) {
+     * try {
+     * Class.forName("com.mysql.jdbc.Driver");
+     * Connection con =
+     * DriverManager.getConnection("jdbc:mysql://localhost:3306/wowdr", "root", "");
+     * Statement stmt = con.createStatement();
+     * String q = "INSERT INTO `items_icons`(`id`, `url`) VALUES ('" + it.getId() +
+     * "','" + it.getNombre() + "','"
+     * + it.getRanura() + "','" + it.getTipo() + "','" + it.getQuality() + "')";
+     * 
+     * // int rs = stmt.executeUpdate(q);
+     * stmt.executeUpdate(q);
+     * 
+     * con.close();
+     * } catch (Exception e) {
+     * System.out.println(e);
+     * }
+     * }
+     * 
+     * public void saveItemIcon(ItemIcon ii) {
+     * try {
+     * Class.forName("com.mysql.jdbc.Driver");
+     * Connection con =
+     * DriverManager.getConnection("jdbc:mysql://localhost:3306/wowdr", "root", "");
+     * Statement stmt = con.createStatement();
+     * String q = "INSERT INTO `items_icons`(`id`, `url`) VALUES ('" + ii.getId() +
+     * "','" + ii.getUrl() + "')";
+     * 
+     * // int rs = stmt.executeUpdate(q);
+     * stmt.executeUpdate(q);
+     * 
+     * con.close();
+     * } catch (Exception e) {
+     * System.out.println(e);
+     * }
+     * }
+     */
 
 }
